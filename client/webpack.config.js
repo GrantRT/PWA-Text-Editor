61b1a0f265bdd7e5694362d1dc41a9dbfea746c8
GrantRT <granto_92@hotmail.com>
2022-05-11 14:59:50 +0100

    Implement asset caching & install logic

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles.
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'JATE',
+        title: 'jate',
       }),
 
       // Injects our custom service worker
@@ -32,9 +32,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Just Another Text Editor',
-        short_name: 'JATE',
-        description: 'A text editor that you can use offline',
+        name: 'pwa-text-editor',
+        short_name: 'jate',
+        description: 'A text editor that you can use online and offline',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
